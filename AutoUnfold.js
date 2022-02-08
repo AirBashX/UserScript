26fa108f9179db1a8c40d319691f9be4f389d9cd
airbash <294379349@qq.com>
2022-02-08 16:32:51 +0800

    适配`百度知道`

--- AutoUnfold.js
+++ AutoUnfold.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         自动展开
 // @description  自动展开文档	隐藏部分
-// @version      1.0.1
+// @version      1.0.2
 // @namespace    https://github.com/AirBashX/AutoUnfold/
 // @author       airbash
 // @match      	 *://jianshu.com/p/*
 // @match		 *://blog.csdn.net/*
 // @match		 *://jingyan.baidu.com/article*
+// @match		 *://zhidao.baidu.com/question*
 // @grant        none
 // @license      GPL-3.0
 // ==/UserScript==
@@ -33,25 +34,65 @@
 				}
 			]
 
+		},
+		{
+			name: "百度知道",
+			url: 'zhidao.baidu.com/question',
+			handles: [
+				{
+					handle: 'disaplay_item',
+					type: "class",
+					item: "wgt-best-mask"
+				},
+				{
+					handle: 'disaplay_item',
+					type: "class",
+					item: "wgt-answers-mask"
+				},
+				{
+					handle: 'click_item',
+					type: "class",
+					item: "w-detail-display-btn"
+				},
+				{
+					handle: 'click_item',
+					type: "id",
+					item: "show-answer-hide"
+				}
+			]
 		}
 	];
 
 	var website = GetWebsite();
-	for (var handle of website.handles) {
-		var items = GetItemElement(handle);
-		if (handle.handle == 'disaplay_item') {
-			//使用css的display:none;隐藏遮挡部分
-			for (var item of items) {
-				item.style.display = "none";
-			}
-		} else {
-			for (var item of items) {
-				item.style.height='unset';
-				item.style.maxHeight='unset';
-				item.style.maxHeight='unset';
+	var interval = setInterval(() => {
+		for (var handle of website.handles) {
+			var items = GetItemElement(handle);
+			if (handle.handle == 'disaplay_item') {
+				//使用css的display:none;隐藏遮挡部分
+				for (var item of items) {
+					item.style.display = "none";
+				}
+			} else if (handle.handle == 'heiht_item') {
+				//加长内容部分
+				for (var item of items) {
+					item.style.height = 'unset';
+					item.style.maxHeight = 'unset';
+					item.style.maxHeight = 'unset';
+				}
+			} else {
+				for (var item of items) {
+					if (
+						item != null &&
+						item.getAttribute("opened") != "yes"
+					) {
+						console.log(item);
+						item.click();
+						item.setAttribute("opened", "yes");
+					}
+				}
 			}
 		}
-	}
+	}, 100);
 
 
 	/**
