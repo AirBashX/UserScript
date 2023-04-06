6ad95d74d618525903d8a36772b8c9450da40b12
airbash <294379349@qq.com>
2023-04-07 06:18:30 +0800

    增加`腾讯视频`、`优酷视频`、`爱奇艺`、`哔哩哔哩`、`西瓜视频`、`欢喜首映`链接;新增组别关闭按钮

--- DoubanAssistant.user.js
+++ DoubanAssistant.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         豆瓣助手
-// @version      0.0.5
+// @version      0.0.6
 // @homepageURL  airbash/DoubanAssistant
 // @homepage     https://github.com/AirBashX/UserScript
 // @author       airbash
-// @description  恢复IMDB的链接,以及增加快捷搜索SubHD、字幕库、射手网、WebHD、rargb、6V电影网中资源的功能
+// @description  恢复IMDB的链接,以及增加快捷搜索SubHD、字幕库、射手网、WebHD、rargb、6V电影网、腾讯视频、优酷视频、爱奇艺、哔哩哔哩、西瓜视频、欢喜首映中资源的功能
 // @match        *://movie.douban.com/subject/*
 // @connect      www.hao6v.tv
 // @grant        GM_registerMenuCommand
@@ -12,18 +12,21 @@
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
 // @license      GPL-3.0
 // @icon         https://img1.doubanio.com/favicon.ico
-// @require      https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js
-// @require      https://cdn.jsdelivr.net/npm/encode-gb2312/encodeToGb2312.min.js
+// @require      https://fastly.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js
+// @require      https://fastly.jsdelivr.net/npm/encode-gb2312@0.0.2/encodeToGb2312.min.js
 // @run-at       document-end
 // ==/UserScript==
-/* global encodeToGb2312 */
+/* global encodeToGb2312 Swal*/
 ///<reference path="./tampermonkey-reference.d.ts" />
+///<reference path="./sweetalert2.d.ts" />
 (function () {
 	("use strict");
 	const url = location.href;
 	const head = document.head;
+
 	//获取imdbitem
 	let info_item = document.querySelector("#info");
 	let imdb_item = document.evaluate('//span[text()="IMDb:"]', info_item, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
@@ -65,6 +68,7 @@
 	 */
 	const webSites = [
 		{
+			id: "group1",
 			name: "字幕搜索",
 			links: [
 				{
@@ -88,6 +92,7 @@
 			],
 		},
 		{
+			id: "group2",
 			name: "影视资源",
 			links: [
 				{
@@ -111,67 +116,95 @@
 				},
 			],
 		},
+		{
+			id: "group3",
+			name: "正版资源",
+			links: [
+				{
+					name: "腾讯视频",
+					url: "v.qq.com",
+					search: "https://v.qq.com/x/search/?q=",
+					id: douban_cn_name,
+				},
+				{
+					name: "优酷视频",
+					url: "youku.com",
+					search: "https://so.youku.com/search_video/q_",
+					id: douban_cn_name,
+				},
+				{
+					name: "爱奇艺",
+					url: "iqiyi.com",
+					search: "https://so.iqiyi.com/so/q_",
+					id: douban_cn_name,
+				},
+				{
+					name: "哔哩哔哩",
+					url: "bilibili.com",
+					search: "https://search.bilibili.com/all?keyword=",
+					id: douban_cn_name,
+				},
+				{
+					name: "西瓜视频",
+					url: "www.ixigua.com",
+					search: "https://www.ixigua.com/search/",
+					id: douban_cn_name,
+				},
+				{
+					name: "欢喜首映",
+					url: "www.huanxi.com",
+					search: "https://www.huanxi.com/search.shtml?sv=",
+					id: douban_cn_name,
+				},
+			],
+		},
 	];
 
-	/**
-	 * 注册油侯菜单
-	 */
-	if (GM_getValue("resource", true) == true) {
-		aside();
-		GM_registerMenuCommand("√侧边栏", () => {
-			GM_setValue("resource", false);
-			location.reload();
-		});
-	} else {
-		GM_registerMenuCommand("X侧边栏", () => {
-			GM_setValue("resource", true);
-			location.reload();
-		});
-	}
+	aside();
 
 	/**
 	 * 配置
 	 */
-	// GM_registerMenuCommand("配置", () => {
-	// 	configuration();
-	// });
+	GM_registerMenuCommand("配置", () => {
+		configuration();
+	});
 
 	/**
 	 * 侧边栏功能
 	 */
 	function aside() {
 		let aside = document.querySelector(".aside");
-
 		for (let webSite of webSites) {
 			let div = document.createElement("div");
 			div.className = "resource";
-			div.innerHTML = "<h2><i>" + webSite.name + "</i>· · · · · ·</h2>";
-			aside.prepend(div);
-			let ul = document.createElement("ul");
-			ul.className = "resources";
-			div.appendChild(ul);
-			for (let link of webSite.links) {
-				if (link.type == "xhr") {
-					GM_xmlhttpRequest({
-						url: link.search,
-						method: "post",
-						headers: {
-							"Content-Type": "application/x-www-form-urlencoded",
-						},
-						timeout: 5000,
-						anonymous:true,
-						data: "show=title%2Csmalltext&tempid=1&tbname=Article&keyboard=" + link.id + "&Submit22=%E6%90%9C%E7%B4%A2",
-						onload: function () {
-							console.log(this.finalUrl);
-							let str = '<a href="' + this.finalUrl + '" target="_blank">' + link.name + "</a>";
-							let a= document.createRange().createContextualFragment(str);
-							ul.appendChild(a);
-						},
-					});
-				} else {
-					let str = '<a href="' + link.search + link.id + '" target="_blank">' + link.name + "</a>";
-					let a= document.createRange().createContextualFragment(str);
-					ul.appendChild(a);
+			if (GM_getValue(webSite.id, true)) {
+				div.innerHTML = "<h2><i>" + webSite.name + "</i>· · · · · ·</h2>";
+				aside.prepend(div);
+				let ul = document.createElement("ul");
+				ul.className = "resources";
+				div.appendChild(ul);
+				for (let link of webSite.links) {
+					if (link.type == "xhr") {
+						GM_xmlhttpRequest({
+							url: link.search,
+							method: "post",
+							headers: {
+								"Content-Type": "application/x-www-form-urlencoded",
+							},
+							timeout: 5000,
+							anonymous: true,
+							data: "show=title%2Csmalltext&tempid=1&tbname=Article&keyboard=" + link.id + "&Submit22=%E6%90%9C%E7%B4%A2",
+							onload: function () {
+								let str = '<a href="' + this.finalUrl + '" target="_blank">' + link.name + "</a>";
+								let a = document.createRange().createContextualFragment(str);
+								ul.appendChild(a);
+							},
+						});
+					} else {
+						let str = '<a href="' + link.search + link.id + '" target="_blank">' + link.name + "</a>";
+						let a = document.createRange().createContextualFragment(str);
+						ul.appendChild(a);
+					}
 				}
 			}
 		}
@@ -185,28 +218,37 @@
 	/**
 	 * 配置
 	 */
-	// function configuration() {
-	// 	Swal.fire({
-	// 		title: "豆瓣助手 配置",
-	// 		html: '<input type="checkbox" class="checkboxs">侧边栏',
-	// 		showCloseButton: true,
-	// 		didRender: () => {
-	// 			if (GM_getValue("resource")) {
-	// 				document.querySelector(".checkboxs").checked = true;
-	// 			}
-	// 		},
-	// 	}).then((result) => {
-	// 		if (result.isConfirmed) {
-	// 			if (document.querySelector(".checkboxs").checked == GM_getValue("resource")) {
-	// 				return;
-	// 			}
-	// 			if (document.querySelector(".checkboxs").checked) {
-	// 				GM_setValue("resource", true);
-	// 			} else {
-	// 				GM_setValue("resource", false);
-	// 			}
-	// 			location.reload();
-	// 		}
-	// 	});
-	// }
+	function configuration() {
+		Swal.fire({
+			title: "豆瓣助手 配置",
+			html: html,
+			showCloseButton: true,
+			didRender: () => {
+				GM_addStyle('.swal2-html-container{text-align:left !important;line-height:unset !important;}.smail_div{width:33%;float:left;}.switch{float:left;position:relative;top:3px;width:40px;height:20px;display:flex;}.checkbox{z-index:3;position:relative;width:100%;height:100%;cursor:pointer;opacity:0;}.bt{z-index:2;position:absolute;top:0;bottom:0;}.bt:before{position:absolute;top:2.5px;left:2.5px;content:"";width:15px;height:15px;background-color:red;border-radius:50%;transition:0.3s cubic-bezier(0.18,0.89,0.35,1.15) all;}.checkbox:checked + .bt:before{left:20px;background-color:#03a9f4;}.bg{z-index:1;position:absolute;top:0;right:0;bottom:0;left:0;border-radius:100px;background-color:#fcebeb;}.checkbox:checked ~ .bg{background-color:#ebf7fc;}')
+				for (let webSite of webSites) {
+					if (GM_getValue(webSite.id, true)) {
+						document.querySelector("#DA_div #" + webSite.id).checked = true;
+					}
+				}
+			},
+		}).then((result) => {
+			let change;
+			if (result.isConfirmed) {
+				for (let webSite of webSites) {
+					if (document.querySelector("#DA_div #" + webSite.id).checked != GM_getValue(webSite.id, true)) {
+						GM_setValue(webSite.id, document.querySelector("#DA_div #" + webSite.id).checked);
+						change=true;
+					}
+				}
+				if(change){
+					location.reload();
+				}
+			}
+		});
+	}
+	let html = "<div id='DA_div'>";
+	for (let webSite of webSites) {
+		html += '<div class="smail_div"><div class="switch"><input type="checkbox" class="checkbox" id="' + webSite.id + '"/><div class="bt"></div><div class="bg"></div></div>'+ webSite.name+'</div>'
+	}
+	html += "</div>";
 })();
