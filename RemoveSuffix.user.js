f6d28684a14c8ca762a3d3aee3974588fe00307f
airbash <294379349@qq.com>
2023-08-03 14:06:48 +0800

    适配`力扣`;修复`CSDN`+`B站专栏`失效的问题

--- RemoveSuffix.user.js
+++ RemoveSuffix.user.js
@@ -1,28 +1,39 @@
 // ==UserScript==
 // @name         移除复制后缀
-// @version      1.0.0
+// @version      1.0.2
 // @namespace    airbash/RemoveSuffix
 // @homepage     https://github.com/AirBashX/UserScript
 // @author       airbash
-// @description  移除复制后缀,手机、电脑全平台通用,长期维护:CSDN、简书、掘金、知乎专栏、B站专栏
+// @description  移除复制后缀,手机、电脑全平台通用,长期维护:CSDN、简书、掘金、知乎专栏、B站专栏、力扣
 // @match        *://*.csdn.net/*
 // @match        *://*.juejin.cn/*
 // @match      	 *://*.jianshu.com/*
 // @match        *://www.zhihu.com/*
 // @match        *://www.bilibili.com/read/mobile*
-// @run-at       document-body
+// @match        *://leetcode.cn/*
+// @run-at       document-start
 // @grant        none
 // @license      GPL-3.0
 // ==/UserScript==
 
 (function () {
-	function addLink(e) {
-		//删除默认事件
-		e.preventDefault();
-		//选择区域
-		var copytext = window.getSelection().toString();
-		//添加到剪切板
-		navigator.clipboard.writeText(copytext);
+	"use strict";
+
+	function copy() {
+		document.addEventListener(
+		"copy",
+		function (event) {
+			event.stopPropagation();
+		},
+		true
+	);
 	}
-	document.addEventListener("copy", addLink);
+
+	function userSelect() {
+		let style = document.createElement("style");
+		style.append("*{user-select:auto !important}");
+		document.head.append(style);
+	}
+	copy();
+	userSelect();
 })();
