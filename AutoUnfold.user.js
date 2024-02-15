7c68fa800569f263810a0b801e9b40e0154fac63
airbash <294379349@qq.com>
2024-02-16 02:53:05 +0800

    适配`网易大神`

--- AutoUnfold.user.js
+++ AutoUnfold.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         自动展开
-// @version      1.3.67
+// @version      1.3.68
 // @namespace    https://github.com/AirBashX/AutoUnfold/
 // @homepageURL  https://github.com/AirBashX/UserScript
 // @author       airbash
-// @description  自动展开文档	隐藏部分;长期维护、PC+手机全平台支持;全平台支持:CSDN、编程之家、简书、知乎、百家号、百度资讯、百度百科、百度经验、百度知道、百度贴吧、百度新闻、新浪新闻、腾讯新闻、搜狐新闻、网易新闻、凤凰新闻、澎湃新闻、新京报、央广网、环球网、人民日报、人民网、今日头条、丁香园、健康界、36氪、果壳、虎扑、虎嗅、头条、B站专栏、B站笔记、微博文章、豆瓣文章、豆瓣小组、开源中国、阿里云开发者社区、腾讯云开发者社区、华为云开发者社区、360图书馆、太平洋电脑网、中关村在线、汽车之家、游侠网、游民星空、360问答、天眼查、天涯社区、东方财富网、喜马拉雅、it1352、代码随想录
+// @description  自动展开文档	隐藏部分;长期维护、PC+手机全平台支持;全平台支持:CSDN、编程之家、简书、知乎、百家号、百度资讯、百度百科、百度经验、百度知道、百度贴吧、百度新闻、新浪新闻、腾讯新闻、搜狐新闻、网易新闻、凤凰新闻、澎湃新闻、新京报、央广网、环球网、人民日报、人民网、今日头条、丁香园、健康界、36氪、果壳、虎扑、虎嗅、头条、B站专栏、B站笔记、微博文章、豆瓣文章、豆瓣小组、开源中国、阿里云开发者社区、腾讯云开发者社区、华为云开发者社区、360图书馆、太平洋电脑网、中关村在线、汽车之家、游侠网、游民星空、网易大神、360问答、天眼查、天涯社区、东方财富网、喜马拉雅、it1352、代码随想录
 // @match        *://*.blog.csdn.net/*
 // @match        *://blog.csdn.net/*
 // @match        *://ask.csdn.net/questions/*
@@ -59,6 +59,7 @@
 // @match        *://club.m.autohome.com.cn/bbs/*
 // @match        *://3g.ali213.net/*
 // @match        *://wap.gamersky.com/*
+// @match        *://m.ds.163.com/*
 // @match        *://wenda.so.com/q/*
 // @match        *://m.wenda.so.com/q/*
 // @match        *://n.tianyancha.com/content/*
@@ -1092,6 +1093,21 @@
 				{
 					type: "height",
 					item: "#gsAreaContext",
+				},
+			],
+		},
+		{
+			name: "网易大神",
+			url: "m.ds.163.com/",
+			handles: [
+				//展开全文
+				{
+					type: "display",
+					item: ".feed-page-main-controller",
+				},
+				{
+					type: "height",
+					item: ".feed-page__main-content",
 				},
 			],
 		},
